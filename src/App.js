8966de2abebb010ce0aa50c805d351f2d149db1f
delightteekay <162933257+delightteekay@users.noreply.github.com>
2024-08-03 03:04:09 +0200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
 return (
 <div className="App">
+<div className='container'>
+<Weather defaultCity="Thohoyandou" />
 <footer>
 This project is coded by{" "} 
 <a href='https://github.com/' target="_blank" rel="noopener noreferrer">
@@ -12,6 +15,7 @@ Delight Tshirando</a>
 <a href='https://github.com/delightteekay/react-final-app' target='_blank'
 rel="noopener noreferrer">open-sourced on GitHub</a>
 </footer>
+</div>
 </div>
 );
 }
