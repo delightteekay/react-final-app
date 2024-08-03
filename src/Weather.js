734f598832896430947f93a76ea03a2babf6a27a
delightteekay <162933257+delightteekay@users.noreply.github.com>
2024-08-03 03:46:46 +0200

    finished with the touch ups

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
+import WeatherForecast from "./WeatherForecast.js";
 import axios from "axios";
 import "./Weather.css";
 
