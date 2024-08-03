549c827e2643e13746ede2d46fb461d4e4a01a6a
delightteekay <162933257+delightteekay@users.noreply.github.com>
2024-08-03 02:39:25 +0200

    added the GitHub repo Url

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@
 import './App.css';
 
 export default function App() {
-  return (
-    <div className="App">
-    Weather App
-    </div>
-  );
+return (
+<div className="App">
+<footer>
+This project is coded by{" "} 
+<a href='https://github.com/' target="_blank" rel="noopener noreferrer">
+Delight Tshirando</a>
+{" "}and is{" "} 
+<a href='https://github.com/delightteekay/react-final-app' target='_blank'
+rel="noopener noreferrer">open-sourced on GitHub</a>
+</footer>
+</div>
+);
 }
 
 
